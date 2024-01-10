cc0829f5d63c54dbc1a875364044fbfcf7402fe5
haile12345 <antenehhayle123@gmail.com>
2024-01-10 09:46:13 -0800

    i did it

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _print_rev_recursion - prints a string, followed by a new line
+ *
+ * @s: revers string to print
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == "\0")
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
