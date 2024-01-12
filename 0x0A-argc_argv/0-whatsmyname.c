4af1fbbac3e03f7ae29a0a990840b6cde3aa9997
haile12345 <antenehhayle123@gmail.com>
2024-01-12 10:52:40 -0800

    let's go

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+
+/**
+ * main - Write a program that prints its name, followed by a new line.
+ *
+ * @argc: length of argumet
+ *
+ * @argv : actuall value of argumet
+ *
+ * Return: allways 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
