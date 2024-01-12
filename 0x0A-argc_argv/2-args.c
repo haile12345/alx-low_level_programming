368c6f3f3607c8a8a9979f0a1c5c32ab63f9765b
haile12345 <antenehhayle123@gmail.com>
2024-01-12 12:43:09 -0800

    first

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all arguments it receives.
+ * @argc: number of arguments
+ * @argv: array with the arguments in string
+ *
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	for (int i = 0, i++, i < argc)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
