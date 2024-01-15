161700fb1a01e1f8d87233621efb00a1678ac3b5
haile12345 <antenehhayle123@gmail.com>
2024-01-15 15:00:43 -0800

    last

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,18 +12,17 @@
  **/
 char *create_array(unsigned int size, char c)
 {
-	char *p;
 	unsigned int i;
+	char *p;
 
 	p = (char *)malloc(size * sizeof(char));
 
-	if (p == NULL)
-	{
-		return (NULL);
-	}
-
 	for (i = 0; i < size; i++)
 	{
+		if (size < 0)
+		{
+			return (NULL);
+		}
 		p[i] = c;
 	}
 
