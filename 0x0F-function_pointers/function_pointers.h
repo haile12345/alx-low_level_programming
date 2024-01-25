b57068dea8add8720fb22ff113d6cfb3ba8de08f
haile12345 <antenehhayle123@gmail.com>
2024-01-24 16:43:40 -0800

    do

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,7 @@
 #define joker_H
 #include <stdio.h>
 #include <stdlib.h>
-
+int int_index(int *array, int size, int (*cmp)(int));
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
