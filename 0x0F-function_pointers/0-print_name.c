4a956ff28d527788004b45d284a684f087e6e8c0
haile12345 <antenehhayle123@gmail.com>
2024-01-24 16:06:59 -0800

    is

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function that prints a name of function.
+ * @name: Name to be printed.
+ * @f: Pointer to a function that takes a char pointer as a parameter.
+ *
+ * Return: Always 0.
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef joker_H
+#define joker_H
+void print_name(char *name, void (*f)(char *));
+#endif
